2a89dab4a4e4c984e5a79efb6dba86fad7e64362
Saikrishna-Jdev <sairaju.jdev@gmail.com>
2021-07-07 18:20:27 +0530

    Adding JPA Repository.Closes #3.

---
+++ src/main/java/com/sai/webservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.sai.webservice.repositories;
+
+import com.sai.webservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
